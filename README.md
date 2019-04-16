a2c2e94035f2518a3784792153a0541fe692b870
christopher mochizuki <mochic808@gmail.com>
2019-04-16 11:49:58 -0700

    qol?

--- README.md
+++ README.md
@@ -5,3 +5,8 @@ simple cookiecutter for bootstrapping python packages...
 ## use
 
     >>> cookiecutter https://github.com/mochic/cookiecutter_thing.git
+
+## inspired
+
+- [ionel](https://blog.ionelmc.ro/2014/05/25/python-packaging/)
+- [requests](https://github.com/kennethreitz/requests)
--- {{cookiecutter.package_name}}/setup.py
+++ {{cookiecutter.package_name}}/setup.py
@@ -1,17 +1,25 @@
 from glob import glob
-from os.path import basename, splitext
+from os import path
 from setuptools import find_packages, setup
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'READNME.md'), 'r') as rstream:
+    readme = rstream.read()
 
 
 setup(
     name='{{cookiecutter.package_name}}',
     version='{{cookiecutter.version}}',
     license='{{cookiecutter.license}}',
+    author='{{cookiecutter.author}}',
+    author_email='{{cookiecutter.author_email}}',
+    long_description=readme,
     description='{{cookiecutter.description}}',
     packages=find_packages('src'),
     package_dir={'': 'src', },
     py_modules=[
-        splitext(basename(path))[0]
+        path.splitext(path.basename(path))[0]
         for path in glob('src/*.py')
     ],
     include_package_data=True,
